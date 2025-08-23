1ed8dcd55f3ee26dbec2bad9754ba7e5c56652b4
minhld <minhld38@gmail.com>
2025-08-23 12:47:52 -0500

    added ship and settings

--- alien _invasion.py
+++ alien _invasion.py
@@ -1,25 +1,29 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
-        self.bg_color = (230, 230, 230)
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.width, self.settings.height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
 
         while True:
-            # Watch for keyboard and mouse events.
+            # Watch for keyboard and mouse events.ship.bmp
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blit_me()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
