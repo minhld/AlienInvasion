065ee86daaafd92423632aa29820bad265d4a5cd
minhld <minhld38@gmail.com>
2025-08-29 00:02:11 -0500

    added key responses for all arrow keys

--- alien _invasion.py
+++ alien _invasion.py
@@ -29,7 +29,16 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     # Move the ship to the right.
-                    self.ship.rect.x += 1
+                    self.ship.rect.x += 3
+                elif event.key == pygame.K_LEFT:
+                    # Move the ship to the right.
+                    self.ship.rect.x -= 3
+                elif event.key == pygame.K_UP:
+                    # Move the ship to the right.
+                    self.ship.rect.y -= 3
+                elif event.key == pygame.K_DOWN:
+                    # Move the ship to the right.
+                    self.ship.rect.y += 3
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
