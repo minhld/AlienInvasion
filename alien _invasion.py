653842a229378755d88b92d78deff0080d00d967
minhld <minhld38@gmail.com>
2025-09-03 23:58:01 -0500

    add small changes

--- alien _invasion.py
+++ alien _invasion.py
@@ -22,7 +22,7 @@ class AlienInvasion:
             self.clock.tick(60)
 
     def _check_events(self):
-        """Respond to keypresses and mouse events."""
+        """Respond to key presses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
